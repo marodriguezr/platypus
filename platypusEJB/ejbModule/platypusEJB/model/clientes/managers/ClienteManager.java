988b049b20dbdcc3cdb0784fa6a5f3108b848139
freddy <freddy@localhost>
2021-03-09 21:36:27 -0500

    Creacion de Clase Manager

---
+++ platypusEJB/ejbModule/platypusEJB/model/clientes/managers/ClienteManager.java
@@ -0,0 +1,20 @@
+package platypusEJB.model.clientes.managers;
+
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+
+/**
+ * Session Bean implementation class ClienteManager
+ */
+@Stateless
+@LocalBean
+public class ClienteManager {
+
+    /**
+     * Default constructor. 
+     */
+    public ClienteManager() {
+        // TODO Auto-generated constructor stub
+    }
+
+}
